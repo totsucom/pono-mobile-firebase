5f712bdd74bef1a956e814a29bc7449fe338d67a
totsucom <maeoka8bit@gmail.com>
2020-09-01 18:37:52 +0900

    Change generate thumb trigger from Storage to Firestore

--- functions/index.js
+++ functions/index.js
@@ -4,233 +4,268 @@ const functions = require('firebase-functions');
 const mkdirp = require('mkdirp');
 const admin = require('firebase-admin');
 admin.initializeApp();
-const spawn = require('child-process-promise').spawn;
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
-/*
-import { runWith, storage, firestore as _firestore } from 'firebase-functions';
-import mkdirp from 'mkdirp';
-import { initializeApp, firestore as __firestore, storage as _storage } from 'firebase-admin';
-initializeApp();
-const firestore = __firestore;
-import { spawn } from 'child-process-promise';
-import { dirname, basename, normalize, join } from 'path';
-import { tmpdir } from 'os';
-import { unlinkSync, writeFileSync } from 'fs';
-*/
+//const { info } = require('console');
+const { createCanvas, loadImage } = require('canvas');
+const ExifImage = require('exif').ExifImage;
 
-// サムネイルの最大サイズ
-const THUMB_MAX_HEIGHT = 200;
-const THUMB_MAX_WIDTH = 200;
+
+// サムネイルのサイズ
+const THUMB_SIZE = 200;
 
 // サムネイルファイル名に付けるプレフィックス名
 const THUMB_PREFIX = 'thumb_';
 
 
+// 署名付きURLを取得する
+async function getUrlFromFile(bucketFile) {
+  const result = await bucketFile.getSignedUrl(
+    {action: 'read', expires: '03-01-2500'});
+  return result[0];
+}
+
+
+
 /*
- * ベース写真がアップロードされたらサムネイルを生成し、Firestoreに情報を書き出す
+ * basePicturesコレクションに変更があった場合に呼び出される
+ * 
  */
-exports.generateThumbnail = functions.runWith({
-  timeoutSeconds: 60, //60秒はデフォルト
-  memory: '1GB'       //ImageMagick の-auto-orientオプションでは256MBで不足
-}).storage.object().onFinalize(async (object) => {
+exports.handleImagesForBasePictures = functions.firestore
+  .document('basePictures/{documentID}').onWrite(async (change, context) => {
 
-  // サムネイル情報を書き出す、Firestoreのコレクション名
-  const COLLECTION_NAME = 'thumbnails';
-
-  // File and directory paths.
-  const filePath = object.name;
-  const contentType = object.contentType; // This is the image MIME type
-  const fileDir = path.dirname(filePath);
-  const fileName = path.basename(filePath);
-  const thumbFilePath = path.normalize(path.join(fileDir, `${THUMB_PREFIX}${fileName}`));
-  const tempLocalFile = path.join(os.tmpdir(), filePath);
-  const tempLocalDir = path.dirname(tempLocalFile);
-  const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath);
-
-  if (['basePictures'].indexOf(fileDir) < 0) {
-    return console.log('generateThumbnail: 監視フォルダでない');
+  if (change.after.exists) {
+    if (!change.before.exists) {
+      //ベース写真が追加された
+      console.log('ベース写真が追加された');
+      const message = await generateBasePictureImage(change.after.data(), change.after.id);
+      return console.log('handleImagesForBasePictures: ' + message);
+    } else {
+      //ベース写真が更新された
+      //写真ありきのコレクションなので、写真が変更されることはない
+      return console.log('handleImagesForBasePictures: DB更新は処理しない');
+    }
+  } else {
+    //ベース写真が削除された
+    console.log('ベース写真が削除された');
+    const message = await deleteProblemImages(change.before.data(), change.before.id);
+    return console.log('handleImagesForBasePictures: ' + message);
   }
+});
 
-  if (!contentType.startsWith('image/')) {
-    return console.log('generateThumbnail: イメージファイルでない');
-  }
-
-  if (fileName.startsWith(THUMB_PREFIX)) {
-    return console.log('generateThumbnail: サムネイルファイルには処理を行わない');
-  }
-
-  // Cloud Storage files.
-  const bucket = admin.storage().bucket(object.bucket);
-  const file = bucket.file(filePath);
-  const thumbFile = bucket.file(thumbFilePath);
-  const metadata = {
-    contentType: contentType,
-    // To enable Client-side caching you can set the Cache-Control headers here.
-    // Uncomment below.
-    // 'Cache-Control': 'public,max-age=3600',
-  };
-  
-  // 一時フォルダを作成する
-  await mkdirp(tempLocalDir);
-  
-  // 対象の画像ファイルを FirebaseCloudStorage から一時フォルダにダウンロードする
-  await file.download({destination: tempLocalFile});
-
-  // 一時フォルダ内で ImageMagick を使ってサムネイルを作成する
-  //await spawn('convert', [
-  //    tempLocalFile, '-thumbnail',
-  //    `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile
-  //  ], {capture: ['stdout', 'stderr']});
-  await spawn('convert', [
-      tempLocalFile, '-auto-orient', '-resize',
-      `${THUMB_MAX_WIDTH}x${THUMB_MAX_HEIGHT}>`, tempLocalThumbFile
-    ], {capture: ['stdout', 'stderr']});
-
-  // 作成したサムネイルを FirebaseCloudStorage にアップロードする
-  await bucket.upload(tempLocalThumbFile, {destination: thumbFilePath, metadata: metadata});
-
-  // 一時フォルダ内の一時ファイルを削除する
-  fs.unlinkSync(tempLocalFile);
-  fs.unlinkSync(tempLocalThumbFile);
-
-  // 署名付きURLを取得する
-  const config = {
-    action: 'read',
-    expires: '03-01-2500',
-  };
-  const results = await Promise.all([
-    thumbFile.getSignedUrl(config),
-    file.getSignedUrl(config),
-  ]);
-  const thumbResult = results[0];
-  const originalResult = results[1];
-  const thumbFileUrl = thumbResult[0];
-  const fileUrl = originalResult[0];
-
-  /* ここからFirestoreに独自形式でコレクションを作成 */
-
-  // ファイルパスからドキュメントIDを作成
-  // （これでユーザーアプリケーションはファイルパスからサムネイルデータにたどり着ける）
-  const docID = filePath.replace('\\',':').replace('/',':')
-
-  // Firestore にコレクションを追加
-  await admin.firestore().collection(COLLECTION_NAME).doc(docID).set({
-    path: filePath,
-    originalURL: fileUrl,       //decodeURI()を使ってもFirestoreに格納
-    thumbnailURL: thumbFileUrl, //されるURLは%でエスケープされてしまうので注意
-    createdAt: admin.firestore.FieldValue.serverTimestamp()
+//無いときは undefined
+function getOrientation(file) {
+  console.log(file);
+  return new Promise(function(resolve, reject) {
+    try {
+      new ExifImage({image: file}, function (error, exifData) {
+          if (error)
+            resolve(undefined);
+          else {
+            console.log(exifData);
+            resolve(exifData.image.Orientation);
+          }
+      });
+    } catch (error) {
+      resolve(undefined);
+    }
   });
-  return console.log('generateThumbnail: サムネイルが作成され、Firestore に追加されました。 DocID=' + docID);
-});
+}
 
-/*
- * ベース写真が削除されたらサムネイル画像とFirestoreのサムネイル情報を削除する
- */
-exports.deleteThumbnail = functions.storage.object()
-  .onDelete(async (object) => {
-  
-    // File and directory paths.
-  const filePath = object.name;
-  const contentType = object.contentType; // This is the image MIME type
-  const fileDir = path.dirname(filePath);
-  const fileName = path.basename(filePath);
-  const thumbFilePath = path.normalize(path.join(fileDir, `${THUMB_PREFIX}${fileName}`));
+async function generateBasePictureImage(snap, docID) {
 
-  if (['basePictures'].indexOf(fileDir) < 0) {
-    return console.log('deleteThumbnail: 監視フォルダでない');
-  }
+  const contentType = 'image/png';
+  const extension = '.png';
 
-  if (!contentType.startsWith('image/')) {
-    return console.log('deleteThumbnail: イメージファイルでない');
-  }
+  const COLLECTION_NAME = 'basePictures';
+  //const IMAGE_URL_FIELD_NAME = 'pictureURL';          //write
+  const THUMB_IMAGE_URL_FIELD_NAME = 'thumbnailURL';  //write
 
-  if (fileName.startsWith(THUMB_PREFIX)) {
-    return console.log('deleteThumbnail: サムネイルファイルには処理を行わない');
-  }
+  //元画像のStorage情報
+  const originalStoragePath = snap.picturePath;     //DBから取得
+  const originalName = path.basename(originalStoragePath);
+  const storageDir = path.dirname(originalStoragePath);
 
-  // Cloud Storage files.
+  //元画像のLocal情報
+  const originalLocalPath = path.join(os.tmpdir(), originalName);
+  const localDir = path.dirname(originalLocalPath);
+
+  //サムネイルのStorage情報
+  const thumbName = THUMB_PREFIX + originalName
+    .substr(0, originalName.length - path.extname(originalName).length)
+    + extension;
+  const thumbStoragePath = path.join(storageDir, thumbName);
+
+  //サムネイル像のLocal情報
+  const thumbLocalPath = path.join(os.tmpdir(), thumbName);
+
+  console.log('ストレージパス ' + originalStoragePath + ' ' + thumbStoragePath);
+  console.log('ローカルパス ' + originalLocalPath + ' ' + thumbLocalPath);
+
+  //Storageオブジェクトを作成
+  const bucket = admin.storage().bucket();
+  const originalFile = bucket.file(originalStoragePath);
+  const thumbFile = bucket.file(thumbStoragePath);
+
+  //元画像をダウンロードする
+  //const originalURL = await getUrlFromFile(originalFile);
+  //const originalImage = await loadImage(originalURL);
+  await mkdirp(localDir);
+  await originalFile.download({destination: originalLocalPath});
+  const originalImage = await loadImage(originalLocalPath);
+ 
+  //Exif情報からOrientationを取得
+  const orientation = await getOrientation(originalLocalPath);
+  var angle = 0.0;
+  if (orientation == 3) angle = 180.0;
+  else if (orientation == 6) angle = 90.0;
+  else if (orientation == 8) angle = 270.0;
+
+  const canvas = createCanvas(THUMB_SIZE, THUMB_SIZE);
+  const ctx = canvas.getContext('2d');
+
+  ctx.beginPath();
+  ctx.fillStyle = "rgba(255, 255, 255, 0.0)";
+  ctx.fillRect(0, 0, THUMB_SIZE, THUMB_SIZE);
+  ctx.stroke();
+
+  ctx.translate(THUMB_SIZE/2, THUMB_SIZE/2);
+  ctx.rotate(angle / 180.0 * Math.PI);
+
+  var scale_x = THUMB_SIZE / originalImage.width;
+  var scale_y = THUMB_SIZE / originalImage.height;
+  var scale = (scale_y < scale_x) ? scale_y : scale_x;
+
+  var dw = originalImage.width * scale;
+  var dh = originalImage.height * scale;
+  var dx = -dw / 2;
+  var dy = -dh / 2;
+
+  ctx.drawImage(originalImage, 0, 0, originalImage.width, originalImage.height,
+    dx, dy, dw, dh);
+
+
+  //一時フォルダを作成し、キャンバスの内容をローカルファイルに保存
+  //await mkdirp(localDir);
+  var b64data = canvas.toDataURL(contentType).split(',')[1];
+  var buffer = new Buffer(b64data, 'base64');
+  fs.writeFileSync(thumbLocalPath, buffer);
+
+  //ストレージにアップロードする
+  await bucket.upload(thumbLocalPath,
+    {destination:thumbFile, metadata: {contentType: contentType}});
+
+  //一時ファイルを削除する
+  fs.unlinkSync(thumbLocalPath);
+
+  //URLを取得する
+  const thumbURL = await getUrlFromBucket(thumbFile);
+
+  //DBに保存
+  var data = {};
+  //data[IMAGE_URL_FIELD_NAME] = originalURL;
+  data[THUMB_IMAGE_URL_FIELD_NAME] = thumbURL;
+  await admin.firestore().collection(COLLECTION_NAME).doc(docID).update(data);
+
+  return thumbURL;
+}
+
+async function deleteProblemImages(snap, docID) {
+
+  //元画像のStorage情報
+  const originalStoragePath = snap.basePicturePath;     //DBから取得
+  const originalName = path.basename(originalStoragePath);
+  const storageDir = path.dirname(originalStoragePath);
+
+  //サムネイルのStorage情報
+  const thumbStoragePath = path.normalize(
+    path.join(storageDir, `${THUMB_PREFIX}${originalName}`));
+
+  //Storageオブジェクトを作成
   const bucket = admin.storage().bucket(object.bucket);
-  const file = bucket.file(thumbFilePath);
+  const originalFile = bucket.file(originalStoragePath);
+  const thumbFile = bucket.file(thumbStoragePath);
 
-  // 削除されたファイルの先頭に単純に thumb_ を付けて削除してみる
-  var message;
+  var message = '';
   try {
-    await file.delete();
-    message = 'ファイル削除成功 ';
-  }
-  catch (e) {
-    message = 'ファイル削除失敗 ';
-  }
-
-  // ファイルパスからドキュメントIDを作成
-  const docID = filePath.replace('\\','-').replace('/','-')
-
-  // ドキュメントIDのコレクションを削除してみる
+    await originalFile.delete();
+    message += '元画像を削除しました '
+  } catch (e) { }
   try {
-    await admin.firestore().collection(COLLECTION_NAME).doc(docID).delete();
-    message += 'コレクション削除成功';
-  }
-  catch (e) {
-    message += 'コレクション削除失敗';
-  }
+    await thumbFile.delete();
+    message += 'サムネイルを削除しました '
+  } catch (e) { }
 
-  return console.log('deleteThumbnail: 処理が完了しました。' + message);
-});
+  if (message.length == 0) message = '画像は削除しませんでした';
+  return message;
+}
 
 /*
  * problemコレクションに変更があった場合に呼び出される
  * 
  */
 exports.handleImagesForProblem = functions.firestore
-  .document('problems/{documentID}').onWrite((change, context) => {
+  .document('problems/{documentID}').onWrite(async (change, context) => {
 
   if (change.after.exists) {
     if (!change.before.exists) {
       //課題が追加された
-      updateProblemImages(change.after.data(), change.after.id);
+      console.log('課題が追加された');
+      await updateProblemImages(change.after.data(), change.after.id);
     } else {
       //課題が更新された
       const snap = change.after.data();
-      if (snap.generateImageRequired)
-        updateProblemImages(change.after.data(), change.after.id);
+      if (snap.imageRequired) { //更新時はフラグ次第
+        console.log('課題が更新された');
+        await updateProblemImages(change.after.data(), change.after.id);
+      }
     }
   } else {
     //課題が削除された
-    deleteProblemImages(change.before.data(), change.before.id);
+    console.log('課題が削除された');
+    await deleteProblemImages(change.before.data(), change.before.id);
   }
 
-  return console.log('handleImagesForProblem 終わり');
+  return console.log('handleImagesForProblem 完了');
 });
 
 //キャンバスの内容をファイルに書き出す
-function saveCanvas(canvas, filePath) {
+async function saveCanvas(canvas, filePath) {
   var b64data = canvas.toDataURL("image/jpeg", 0.75).split(',')[1];
   var buffer = new Buffer(b64data, 'base64');
-  writeFileSync(filePath, buffer);
+  fs.writeFileSync(filePath, buffer);
 }
 
 // 署名付きURLを取得する
 async function getUrlFromBucket(bucketFile) {
   const result = await bucketFile.getSignedUrl(
     {action: 'read', expires: '03-01-2500'});
-  return result1[0];
+  return result[0];
 }
 
 async function updateProblemImages(snap, docID) {
-
-  const COLLECTION_NAME = 'problem';
-
-  const filePath = 'problemImages/completedImages/{docID}.jpg';
-  const tempLocalFile = path.join(os.tmpdir(), filePath);
-  const tempLocalDir = path.dirname(tempLocalFile);
-
   const { createCanvas, loadImage } = require('canvas');
 
+  console.log("ID = " + docID);
 
-  const bucket = admin.storage().bucket(object.bucket);
-  const basePictureURL = await getUrlFromBucket(bucket.file(filePath));
+  const COLLECTION_NAME = 'problems';
+  const IMGE_OUTPUT_PATH = 'problemImages/completedImages';
+
+
+  const basePicturePath = snap.basePicturePath;
+  console.log("basePicturePath = " + basePicturePath);
+  const basePictureName = path.basename(basePicturePath);
+
+  const tempLocalFile = path.join(os.tmpdir(), basePictureName);
+  console.log("tempLocalFile = " + tempLocalFile);
+  const tempLocalDir = path.dirname(tempLocalFile);
+
+
+
+  const bucket = admin.storage().bucket();
+  const basePictureURL = await getUrlFromBucket(bucket.file(basePicturePath));
+  console.log("basePictureURL = " + basePictureURL);
   const basePicture = await loadImage(basePictureURL);
 
 
@@ -250,32 +285,38 @@ async function updateProblemImages(snap, docID) {
   // 一時フォルダを作成する
   await mkdirp(tempLocalDir);
 
-  // キャンバスをファイルに保存
-  saveCanvas(canvas, tempLocalFile);
+  // キャンバスを一時ファイルに保存
+  await saveCanvas(canvas, tempLocalFile);
 
-  // Cloud Storage files.
-  const file = bucket.file(filePath);
+  // Storage用のオブジェクトを準備
+  const completedImagePath = `${IMGE_OUTPUT_PATH}/${docID}.jpg`;
+  const completedImageFile = bucket.file(completedImagePath);
   const metadata = {
-    contentType: contentType,
-    // To enable Client-side caching you can set the Cache-Control headers here.
-    // Uncomment below.
-    // 'Cache-Control': 'public,max-age=3600',
+    contentType: 'image/jpeg',//contentType,
+    //'Cache-Control': 'public,max-age=3600', // For client side
   };
 
   // キャンバスファイルを FirebaseCloudStorage にアップロードする
-  await bucket.upload(tempLocalFile, {destination:filePath, metadata: metadata});
+  await bucket.upload(tempLocalFile,
+    {destination:completedImagePath, metadata: metadata});
 
   // 一時フォルダ内の一時ファイルを削除する
   fs.unlinkSync(tempLocalFile);
 
   // 署名付きURLを取得する
-  const fileUrl = getUrlFromBucket(file);
+  const completedImageURL = await getUrlFromBucket(completedImageFile);
+  console.log("completedImageURL = " + completedImageURL);
 
   // Firestore のコレクションを更新
-  await admin.firestore().collection(COLLECTION_NAME).doc(docID).set({
-    completedImageURL: fileUrl, //decodeURI()を使ってもFirestoreに格納
-                                //されるURLは%でエスケープされてしまうので注意
+  /*await snap.ref.set({
+    //decodeURI()を使ってもFirestoreに格納されるURLは%でエスケープされてしまうので注意
+    completedImageURL: completedImageURL,
     generateImageRequired: false
+  });*/
+  await admin.firestore().collection(COLLECTION_NAME).doc(docID).set({
+    //decodeURI()を使ってもFirestoreに格納されるURLは%でエスケープされてしまうので注意
+    completedImageURL: completedImageURL,
+    imageRequired: false
   });
 
   console.log('handleImagesForProblem.updateProblemImages: 完了 ' + docID);
@@ -285,7 +326,7 @@ async function deleteProblemImages(snap, docID) {
   const filePath = 'problemImages/completedImages/{docID}.jpg';
 
   // Cloud Storage files.
-  const bucket = admin.storage().bucket(object.bucket);
+  const bucket = admin.storage().bucket();
   const file = bucket.file(filePath);
   
   try {
@@ -293,93 +334,3 @@ async function deleteProblemImages(snap, docID) {
   } catch (e) {
   }
 }
-
-/*
-export const test = storage.object().onFinalize(async (object) => {
-
-  // サムネイルファイル名に付けるプレフィックス名
-  const THUMB_PREFIX = 'thumb_';
-
-  // サムネイル情報を書き出す、Firestoreのコレクション名
-  const COLLECTION_NAME = 'thumbnails';
-
-  // File and directory paths.
-  const filePath = object.name;
-  const contentType = object.contentType; // This is the image MIME type
-  const fileDir = path.dirname(filePath);
-  const fileName = path.basename(filePath);
-  const thumbFilePath = path.normalize(path.join(fileDir, `${THUMB_PREFIX}${fileName}`));
-  const tempLocalFile = path.join(os.tmpdir(), filePath);
-  const tempLocalDir = path.dirname(tempLocalFile);
-  const tempLocalThumbFile = path.join(os.tmpdir(), thumbFilePath);
-
-  if (['problemImages'].indexOf(fileDir) < 0) {
-    return console.log('test: 監視フォルダでない');
-  }
-
-  if (!contentType.startsWith('image/')) {
-    return console.log('test: イメージファイルでない');
-  }
-
-  if (fileName.startsWith(THUMB_PREFIX)) {
-    return console.log('test: サムネイルファイルには処理を行わない');
-  }
-
-  // Cloud Storage files.
-  const bucket = admin.storage().bucket(object.bucket);
-  const file = bucket.file(filePath);
-  const thumbFile = bucket.file(thumbFilePath);
-  const metadata = {
-    contentType: contentType,
-    // To enable Client-side caching you can set the Cache-Control headers here.
-    // Uncomment below.
-    // 'Cache-Control': 'public,max-age=3600',
-  };
-
-  // 署名付きURLを取得する
-  const result = await file
-    .getSignedUrl({ action: 'read', expires: '03-01-2500' });
-  const fileUrl = result[0];
-
-  const { createCanvas, loadImage } = require('canvas');
-  const basePicture = await loadImage(fileUrl);
-
-    const canvas = createCanvas(basePicture.width, basePicture.height);
-    const ctx = canvas.getContext('2d');
-
-    ctx.drawImage(basePicture, 0, 0);
-
-    ctx.beginPath () ;
-    ctx.moveTo( 0, 0 ) ;
-    ctx.lineTo( 200, 200 );
-    ctx.strokeStyle = "red" ;
-    ctx.lineWidth = 10 ;
-    ctx.stroke() ;
-
-    // 一時フォルダを作成する
-    await mkdirp(tempLocalDir);
-
-    // キャンバスをファイルに保存
-    saveCanvas(canvas, tempLocalFile);
-
-    // 保存したキャンバスファイルを FirebaseCloudStorage にアップロードする
-    await bucket.upload(tempLocalFile, {destination: thumbFilePath, metadata: metadata});
-
-    // 一時フォルダ内の一時ファイルを削除する
-    fs.unlinkSync(tempLocalFile);
-
-    // 署名付きURLを取得する
-    const result1 = await thumbFile.getSignedUrl({action: 'read', expires: '03-01-2500'});
-    const thumbFileUrl =result1[0];
-
-    console.log(thumbFileUrl);
-
-    return console.log('終わり');
-});
-
-function saveCanvas(canvas, filename) {
-  var b64data = canvas.toDataURL().split(',')[1];
-  var buffer = new Buffer(b64data, 'base64');
-  writeFileSync(filename, buffer);
-}
-*/
